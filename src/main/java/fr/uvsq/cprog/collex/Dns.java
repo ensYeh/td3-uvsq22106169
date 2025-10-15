431e94830c1e41c76ec0fc4d7558f95b73c009d9
Boukebouche <camilia.boukebouche@ens.uvsq.fr>
2025-10-15 21:40:18 +0200

    Ajout de la création du fichier DNS si absent

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -14,44 +14,51 @@ public class Dns{
         this.path = path;
 
         try {
-            FileInputStream file = new FileInputStream(path);
-            Scanner scanner = new Scanner(file);
-            
-            while(scanner.hasNextLine()) {
-                String ligne = scanner.nextLine().trim(); 
-                if (ligne.isEmpty()) {
-                    continue;
-                }
-                String[] parts = ligne.split("\\s+");
-                if (parts.length != 2) {
-                    System.err.println("Format invalide");
-                    continue;
-                }
 
-                String ip = parts[0];
-                String nomQualifie = parts[1];
-
-                int indexPoint = nomQualifie.indexOf('.');
-                if (indexPoint == -1) {
-                    System.err.println("Nom qualifié invalide : " + nomQualifie);
-                    continue;   
-                }
-
-                String nom = nomQualifie.substring(0, indexPoint);
-                String domaine = nomQualifie.substring(indexPoint + 1);
-
-                try {
-                    AdresseIP adresse = new AdresseIP(ip);
-                    NomMachine machine = new NomMachine(nom,domaine);
-                    DnsItem item = new DnsItem(machine,adresse);
-                    enregistrements.add(item);
-
-                } catch (IllegalArgumentException e) {
-                    System.err.println("Erreur dans la ligne : " + ligne + " → " + e.getMessage());
-                }
-                
+            File f = new File(path);
+            if (!f.exists()) {
+                f.createNewFile();
             }
-            
+
+            try (FileInputStream file = new FileInputStream(f);
+                Scanner scanner = new Scanner(file) ) {
+                
+                while(scanner.hasNextLine()) {
+                    String ligne = scanner.nextLine().trim(); 
+                    if (ligne.isEmpty()) {
+                        continue;
+                    }
+                    String[] parts = ligne.split("\\s+");
+                    if (parts.length != 2) {
+                        System.err.println("Format invalide");
+                        continue;
+                    }
+
+                    String ip = parts[0];
+                    String nomQualifie = parts[1];
+
+                    int indexPoint = nomQualifie.indexOf('.');
+                    if (indexPoint == -1) {
+                        System.err.println("Nom qualifié invalide : " + nomQualifie);
+                        continue;   
+                    }
+
+                    String nom = nomQualifie.substring(0, indexPoint);
+                    String domaine = nomQualifie.substring(indexPoint + 1);
+
+                    try {
+                        AdresseIP adresse = new AdresseIP(ip);
+                        NomMachine machine = new NomMachine(nom,domaine);
+                        DnsItem item = new DnsItem(machine,adresse);
+                        enregistrements.add(item);
+
+                    } catch (IllegalArgumentException e) {
+                        System.err.println("Erreur dans la ligne : " + ligne + " → " + e.getMessage());
+                    }
+                    
+                }
+        
+            }
         } catch (IOException e) {
             throw new RuntimeException("erreur lors de la lecture" + path,e);
 
@@ -115,9 +122,6 @@ public class Dns{
         } catch (IOException e) {
             System.err.println("Erreur lors de l'écriture dans le fichier : " + e.getMessage());
         }
-
-
-
     }
     
 }
