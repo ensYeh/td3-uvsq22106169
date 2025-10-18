2537186a295d4a6470ed4f6827d39d76a7e0a3e7
Boukebouche <camilia.boukebouche@ens.uvsq.fr>
2025-10-18 18:36:02 +0200

    Ajout de l'exécutable dans la classe DnsApp

---
+++ src/main/java/fr/uvsq/cprog/collex/DnsApp.java
@@ -0,0 +1,39 @@
+package fr.uvsq.cprog.collex;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+
+public class DnsApp {
+
+    public void run() {
+        try {
+            Properties p = new Properties();
+            FileInputStream in = new FileInputStream("src/main/resources/config.properties");
+            p.load(in);
+            in.close();
+            
+            String path = p.getProperty("dnsFile");
+
+            Dns dns = new Dns(path);
+            DnsTUI tui = new DnsTUI(dns);
+
+            while (true) {
+                Commande c = tui.nextCommande();
+                if (c == null) continue;
+                if (c instanceof Quitter) break;
+                c.execute();
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier de configuration : " + e.getMessage());
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        new DnsApp().run();
+    }
+
+}
