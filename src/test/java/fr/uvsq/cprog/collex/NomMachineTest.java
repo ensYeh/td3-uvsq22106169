01e462f2e7135509a08f69c43dcd557ecbe3e38b
Boukebouche <camilia.boukebouche@ens.uvsq.fr>
2025-10-18 20:42:32 +0200

    Modification pour respecter les conventions

--- src/test/java/fr/uvsq/cprog/collex/NomMachineTest.java
+++ src/test/java/fr/uvsq/cprog/collex/NomMachineTest.java
@@ -1,62 +1,67 @@
 package fr.uvsq.cprog.collex;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
+/**
+ * Classe de tests unitaires pour la classe NomMachine.
+ */
+
 public class NomMachineTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_nommachine_vide() {
-        new NomMachine("", "uvsq.fr");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_nommachine_vide() {
+    new NomMachine("", "uvsq.fr");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_domaine_vide() {
-        new NomMachine("www", "");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_domaine_vide() {
+    new NomMachine("www", "");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_nom_domaine_vide() {
-        new NomMachine("", "");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_nom_domaine_vide() {
+    new NomMachine("", "");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_nommachine_null() {
-        new NomMachine(null, "uvsq.fr");
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_nommachine_null() {
+    new NomMachine(null, "uvsq.fr");
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_domaine_null() {
-        new NomMachine("www", null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_domaine_null() {
+    new NomMachine("www", null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_nom_domaine_null() {
-        new NomMachine(null, null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_nom_domaine_null() {
+    new NomMachine(null, null);
+  }
 
-    @Test
-    public void test_NomQualifie() {
-        NomMachine nm = new NomMachine("www", "uvsq.fr");
-        assertEquals("www.uvsq.fr", nm.getNomQualifie());
-    }
+  @Test
+  public void test_NomQualifie() {
+    NomMachine nm = new NomMachine("www", "uvsq.fr");
+    assertEquals("www.uvsq.fr", nm.getNomQualifie());
+  }
 
-    @Test
-    public void test_equals_hashCode() {
-        NomMachine nm1 = new NomMachine("www", "uvsq.fr");
-        NomMachine nm2 = new NomMachine("www", "uvsq.fr");
-        NomMachine nm3 = new NomMachine("poste", "uvsq.fr");
+  @Test
+  public void test_equals_hashCode() {
+    NomMachine nm1 = new NomMachine("www", "uvsq.fr");
+    NomMachine nm2 = new NomMachine("www", "uvsq.fr");
+    NomMachine nm3 = new NomMachine("poste", "uvsq.fr");
 
-        assertEquals(nm1, nm2);
-        assertEquals(nm1.hashCode(), nm2.hashCode());
-        assertNotEquals(nm1, nm3);
-    }
+    assertEquals(nm1, nm2);
+    assertEquals(nm1.hashCode(), nm2.hashCode());
+    assertNotEquals(nm1, nm3);
+  }
 
-    @Test
-    public void test_toString() {
-        NomMachine nm = new NomMachine("www", "uvsq.fr");
-        assertEquals("www.uvsq.fr", nm.toString());
-    }
+  @Test
+  public void test_toString() {
+    NomMachine nm = new NomMachine("www", "uvsq.fr");
+    assertEquals("www.uvsq.fr", nm.toString());
+  }
 
 }
