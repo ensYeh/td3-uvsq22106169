f26580e5420e4f5fb3aafdcd2287adbb9f44edba
Boukebouche <camilia.boukebouche@ens.uvsq.fr>
2025-10-18 21:19:58 +0200

    Changement pour respecter les conventions

--- src/main/java/fr/uvsq/cprog/collex/RechercheParNom.java
+++ src/main/java/fr/uvsq/cprog/collex/RechercheParNom.java
@@ -1,22 +1,25 @@
 package fr.uvsq.cprog.collex;
 
+/**
+ * Commande permettant d'effectuer une recherche d'adresse IP à partir d'un nom de machine.
+ */
 public class RechercheParNom implements Commande {
 
-    private final NomMachine nommachine;
-    private final Dns dns;
+  private final NomMachine nommachine;
+  private final Dns dns;
 
-    public RechercheParNom(NomMachine nommachine, Dns dns) {
-        this.nommachine = nommachine;
-        this.dns = dns;
-    }
+  public RechercheParNom(NomMachine nommachine, Dns dns) {
+    this.nommachine = nommachine;
+    this.dns = dns;
+  }
 
-    @Override
-    public void execute() {
-        AdresseIP ip = dns.getItem(nommachine);
-        if (ip != null) {
-            System.out.println(ip);
-        } else {
-            System.out.println("Machine inconnue : " + nommachine);
-        }
+  @Override
+  public void execute() {
+    AdresseIP ip = dns.getItem(nommachine);
+    if (ip != null) {
+      System.out.println(ip);
+    } else {
+      System.out.println("Machine inconnue : " + nommachine);
     }
+  }
 }
