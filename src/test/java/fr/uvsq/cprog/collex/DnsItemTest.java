ca2993e6fea754c25d84b5a3009c5d652df331a0
Boukebouche <camilia.boukebouche@ens.uvsq.fr>
2025-10-18 21:15:51 +0200

    Modification pour respecter les conventions

--- src/test/java/fr/uvsq/cprog/collex/DnsItemTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsItemTest.java
@@ -1,54 +1,59 @@
 package fr.uvsq.cprog.collex;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
+/**
+ * Classe de tests unitaires pour la classe DnsItem.
+ */
+
 public class DnsItemTest {
     
-    @Test(expected = IllegalArgumentException.class) 
-    public void test_nommachine_null(){
-        new DnsItem(null,new AdresseIP("193.51.31.90"));    
-    }
+  @Test(expected = IllegalArgumentException.class) 
+  public void test_nommachine_null() {
+    new DnsItem(null, new AdresseIP("193.51.31.90"));    
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_adresseIP_null() {
-        new DnsItem(new NomMachine("www", "uvsq.fr"), null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_adresseIp_null() {
+    new DnsItem(new NomMachine("www", "uvsq.fr"), null);
+  }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_adresseIP_NomMachine_null() {
-        new DnsItem(null, null);
-    }
+  @Test(expected = IllegalArgumentException.class)
+  public void test_adresseIp_Nommachine_null() {
+    new DnsItem(null, null);
+  }
 
-    @Test
-    public void test_getters() {
-        NomMachine nm = new NomMachine("www", "uvsq.fr");
-        AdresseIP ip = new AdresseIP("193.51.31.90");
-        DnsItem item = new DnsItem(nm,ip);
+  @Test
+  public void test_getters() {
+    NomMachine nm = new NomMachine("www", "uvsq.fr");
+    AdresseIP ip = new AdresseIP("193.51.31.90");
+    DnsItem item = new DnsItem(nm, ip);
 
-        assertEquals(nm, item.getNomMachineobj());
-        assertEquals(ip, item.getAdresseIpobj());
-    }
+    assertEquals(nm, item.getNomMachineobj());
+    assertEquals(ip, item.getAdresseIpobj());
+  }
 
-    @Test
-    public void test_toString() {
-        NomMachine nm = new NomMachine("www", "uvsq.fr");
-        AdresseIP ip = new AdresseIP("193.51.31.90");
-        DnsItem item = new DnsItem(nm, ip);
+  @Test
+  public void test_toString() {
+    NomMachine nm = new NomMachine("www", "uvsq.fr");
+    AdresseIP ip = new AdresseIP("193.51.31.90");
+    DnsItem item = new DnsItem(nm, ip);
 
-        assertEquals("193.51.31.90 www.uvsq.fr", item.toString());
-    }
+    assertEquals("193.51.31.90 www.uvsq.fr", item.toString());
+  }
 
-    @Test
-    public void test_equals_hashCode() {
-        DnsItem item1 = new DnsItem(new NomMachine("www", "uvsq.fr"), new AdresseIP("193.51.31.90"));
-        DnsItem item2 = new DnsItem(new NomMachine("www", "uvsq.fr"), new AdresseIP("193.51.31.90"));
-        DnsItem item3 = new DnsItem(new NomMachine("poste", "uvsq.fr"), new AdresseIP("193.51.31.90"));
+  @Test
+  public void test_equals_hashCode() {
+    DnsItem item1 = new DnsItem(new NomMachine("www", "uvsq.fr"), new AdresseIP("193.51.31.90"));
+    DnsItem item2 = new DnsItem(new NomMachine("www", "uvsq.fr"), new AdresseIP("193.51.31.90"));
+    DnsItem item3 = new DnsItem(new NomMachine("poste", "uvsq.fr"), new AdresseIP("193.51.31.90"));
 
-        assertEquals(item1, item2);
-        assertEquals(item1.hashCode(), item2.hashCode());
-        assertNotEquals(item1, item3);
-    }
+    assertEquals(item1, item2);
+    assertEquals(item1.hashCode(), item2.hashCode());
+    assertNotEquals(item1, item3);
+  }
 
 }
